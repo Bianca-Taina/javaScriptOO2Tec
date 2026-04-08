9cd1908aae2163c3340e3607fa04f445536890da
Bianca Tainá Lino Balduino <bianca.balduino@escola.pr.gov.br>
2026-04-08 13:06:15 +0000

    22

---
+++ classeAutoHeranca.js
@@ -0,0 +1,36 @@
+class Automovel{
+    constructor(marca, modelo, ano){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    exibirDetalhes(){
+        return `${this.marca}, ${this.modelo}, ${this.ano}`;
+    }
+}
+
+class Carro extends Automovel{
+    constructor(marca, modelo, ano, portas){
+        super(marca, modelo, ano);
+        this.portas = portas;
+    }
+    exibirDetalhes(){
+        return `${super.exibirDetalhes()}, Portas:${this.portas}`;
+    }
+}
+
+class Moto extends Automovel{
+    constructor(marca,modelo, ano, cilindrada){
+        super(marca,modelo, ano);
+        this.cilindrada = cilindrada;
+    }
+    exibirDetalhes(){
+        return`${super.exibirDetalhes()}, Cilindrada:${this.cilindrada}`;
+    }
+}
+
+let moto1 = new Moto(" Kawasaki", "Ninja H2", 2015, 998 );
+console.log(moto1.exibirDetalhes());
+let carro1 = new Carro ("Nissan", "GT-R NISMO", 2024, 2);
+console.log(carro1.)
